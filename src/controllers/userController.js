9faf5a673619bde47353f65493ac0b6874ea5c48
Bilal Azam <bilal.azam1@outlook.com>
2023-01-04 00:00:00 +0000

    Implement basic user registration

---
+++ src/controllers/userController.js
@@ -0,0 +1,24 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
+exports.registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    user = new User({
+      name,
+      email,
+      password: await bcrypt.hash(password, 10),
+    });
+
+    await user.save();
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const { registerUser } = require('../controllers/userController');
+
 const router = express.Router();
 
-// Placeholder route
+router.post('/register', registerUser);
 router.get('/', (req, res) => {
   res.send('User route is working');
 });
