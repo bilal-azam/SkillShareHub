884af6eb42359c190d6e91752ed12b767e0e7d0d
Bilal Azam <bilal.azam1@outlook.com>
2023-02-21 00:00:00 +0000

    Deployed application and set up monitoring

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "your_sentry_dsn",
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
